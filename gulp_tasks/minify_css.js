e2fc4a03d83de1b66cebf36061f48e6ecc00cd27
acr0matic <work.acr0matic@gmail.com>
2022-08-26 14:46:17 +0300

    feat: переработка архитектуры под проект

--- gulp_tasks/minify_css.js
+++ gulp_tasks/minify_css.js
@@ -7,11 +7,6 @@ const paths = require('../gulpfile');
 
 // Задача для минификации кода стилей для сокращения скорости загрузки веб-страницы
 task('minify_css', () => src(paths.styles.temp + paths.styles.out)
-  .pipe(
-    purgecss({
-      content: ['src/**/*.{html,php}', 'src/**/*.js'],
-    }),
-  )
   .pipe(cleanCSS({ level: 2 }))
   .pipe(rename(paths.styles.minify))
   .pipe(dest(paths.styles.dist)));
