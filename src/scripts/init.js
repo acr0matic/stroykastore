d1164c1196d5195d681fe8b28ac136e9e1f51af9
acr0matic <work.acr0matic@gmail.com>
2022-08-31 17:43:51 +0300

    feat: верстка главного экрана

--- src/scripts/init.js
+++ src/scripts/init.js
@@ -10,11 +10,22 @@ const lazyLoadInstance = new LazyLoad({
 });
 
 $(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
+  $(".slider-feedback").owlCarousel({
     dots: false,
     nav: true,
     navContainer: '#feedback .section__header .slider-nav',
     navText: '',
     margin: 16,
+  });
+});
+
+$(document).ready(function () {
+  $(".slider-hero").owlCarousel({
+    dots: false,
+    nav: true,
+    // navContainer: '#feedback .section__header .slider-nav',
+    navText: '',
+    items: 1,
+    loop: true,
   });
 });
