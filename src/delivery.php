4abfb4f996cb2aefb7616e2d6c97f6eb7578cf93
acr0matic <work.acr0matic@gmail.com>
2022-09-06 01:32:55 +0300

    feat: страница "доставка"

---
+++ src/delivery.php
@@ -0,0 +1,325 @@
+<?php include_once('layout/partials/header.php') ?>
+
+<main>
+  <section id="heading" class="section">
+    <div class="heading">
+      <div class="container container--wide">
+        <div class="container">
+          <div class="heading__content">
+            <div class="page__breadcrumb">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="#">Главная</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Доставка</li>
+                </ol>
+              </nav>
+            </div>
+            <!-- /.page__breadcrumb -->
+
+            <div class="page__header">
+              <h1 class="page__title">
+                Доставка
+              </h1>
+            </div>
+            <!-- /.page__header -->
+
+            <p class="page__description">
+              Доставка осуществляется курьерами поставщика
+              <br>
+              или службой курьеров Достависта. Также товар
+              <br>
+              можно забрать самостоятельно от поставщика
+            </p>
+          </div>
+          <!-- /.heading__content -->
+        </div>
+        <!-- /.container -->
+
+        <div style="background-image: url('img/page/delivery/heading/background.jpg');" class="heading__background"></div>
+      </div>
+      <!-- /.container container--wide -->
+    </div>
+    <!-- /.heading -->
+  </section>
+
+  <section id="delivery" class="section">
+    <div class="container">
+      <div class="section__header">
+        <h2 class="section__title">
+          Как сделать заказ: 4 простых шага
+        </h2>
+      </div>
+      <!-- /.section__header -->
+
+      <div class="delivery">
+        <div class="row">
+          <div class="col-3">
+            <div class="delivery__step">
+              <picture class="picture picture--contain delivery__icon">
+                <img class="picture__media" src="img/page/delivery/steps/1.svg" alt="">
+              </picture>
+
+              <p class="delivery__description">
+                Добавьте нужные товары <br> в корзину и оплатите заказ
+              </p>
+            </div>
+            <!-- /.delivery__step -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-3">
+            <div class="delivery__step">
+              <picture class="picture picture--contain delivery__icon">
+                <img class="picture__media" src="img/page/delivery/steps/2.svg" alt="">
+              </picture>
+
+              <p class="delivery__description">
+                Получите уведомления <br> о подтверждении вашего заказа
+              </p>
+            </div>
+            <!-- /.delivery__step -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-3">
+            <div class="delivery__step">
+              <picture class="picture picture--contain delivery__icon">
+                <img class="picture__media" src="img/page/delivery/steps/3.svg" alt="">
+              </picture>
+
+              <p class="delivery__description">
+                После подтверждения <br> мы сформируем ваш заказ </p>
+              </p>
+            </div>
+            <!-- /.delivery__step -->
+          </div>
+          <!-- /.col-3 -->
+
+          <div class="col-3">
+            <div class="delivery__step">
+              <picture class="picture picture--contain delivery__icon">
+                <img class="picture__media" src="img/page/delivery/steps/4.svg" alt="">
+              </picture>
+
+              <p class="delivery__description">
+                Заберите из пунктов <br> выдачи
+            </div>
+            <!-- /.delivery__step -->
+          </div>
+          <!-- /.col-3 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.delivery -->
+    </div>
+    <!-- /.container -->
+  </section>
+
+  <section id="faq" class="section">
+    <div class="container border">
+      <div class="faq">
+        <div class="row justify-content-center">
+          <div class="col-8">
+            <div class="section__header">
+              <h2 class="section__title">
+                Часто задаваемые вопросы
+              </h2>
+            </div>
+            <!-- /.section__header -->
+
+            <div class="accordion" id="accordionEx">
+              <div class="card mb-3">
+                <!-- Card header -->
+                <div class="card-header" id="headingOne1">
+                  <a data-toggle="collapse" data-parent="#accordionEx" href="#collapseOne1" aria-expanded="true">
+                    <h5 class="mb-0">
+                      Водитель уведомит меня о начале доставки доставки?
+                      <i class="icon icon--arrow-d"></i>
+                    </h5>
+                  </a>
+                </div>
+
+                <!-- Card body -->
+                <div id="collapseOne1" class="collapse show" role="tabpanel" aria-labelledby="headingOne1" data-parent="#accordionEx">
+                  <div class="card-body">
+                    Есть товары, которые можно купить сразу на сервисе, и товары, для покупки которых нужно перейти на сайт магазина. Условия их получения отличаются.
+                    <br> <br>
+                    В этом разделе описаны условия для товаров, которые можно купить. Также некоторые товары, которые продаются на Маркете, доставляются непосредственно продавцами
+                    и имеют пометку об этом. Для таких товаров могут быть доступны не все возможности, описанные ниже.
+                  </div>
+                </div>
+
+              </div>
+              <!-- card -->
+
+              <div class="card mb-3">
+                <div class="card-header" id="headingTwo2">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2" aria-expanded="false">
+                    <h5 class="mb-0">
+                      Мне нужна помощь в разгрузке товара (дополнительная услуга), что делать?
+                      <i class="icon icon--arrow-d"></i>
+                    </h5>
+                  </a>
+                </div>
+
+                <!-- Card body -->
+                <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
+                  <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                    labore sustainable VHS.
+                  </div>
+                </div>
+
+              </div>
+              <!-- card -->
+
+              <div class="card mb-3">
+                <div class="card-header" id="headingTwo2">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2" aria-expanded="false">
+                    <h5 class="mb-0">
+                      Заказ доставили, что то от меня требуется?
+                      <i class="icon icon--arrow-d"></i>
+                    </h5>
+                  </a>
+                </div>
+
+                <!-- Card body -->
+                <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
+                  <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                    labore sustainable VHS.
+                  </div>
+                </div>
+
+              </div>
+              <!-- card -->
+
+              <div class="card mb-3">
+                <div class="card-header" id="headingTwo2">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2" aria-expanded="false">
+                    <h5 class="mb-0">
+                      Что делать если товар не тот, или ненадлежащего качества?
+                      <i class="icon icon--arrow-d"></i>
+                    </h5>
+                  </a>
+                </div>
+
+                <!-- Card body -->
+                <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
+                  <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                    labore sustainable VHS.
+                  </div>
+                </div>
+
+              </div>
+              <!-- card -->
+
+
+
+              <div class="card mb-3">
+                <div class="card-header" id="headingTwo2">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2" aria-expanded="false">
+                    <h5 class="mb-0">
+                      У меня крупный заказ (5 и более тонн груза), у вас есть доставка и разгрузка манипулятором?
+                      <i class="icon icon--arrow-d"></i>
+                    </h5>
+                  </a>
+                </div>
+
+                <!-- Card body -->
+                <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
+                  <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                    labore sustainable VHS.
+                  </div>
+                </div>
+
+              </div>
+              <!-- card -->
+
+
+              <div class="card mb-3">
+                <div class="card-header" id="headingTwo2">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseTwo2" aria-expanded="false">
+                    <h5 class="mb-0">
+                      Сколько времени дается на самостоятельную разгрузку товара?
+                      <i class="icon icon--arrow-d"></i>
+                    </h5>
+                  </a>
+                </div>
+
+                <!-- Card body -->
+                <div id="collapseTwo2" class="collapse" role="tabpanel" aria-labelledby="headingTwo2" data-parent="#accordionEx">
+                  <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                    labore sustainable VHS.
+                  </div>
+                </div>
+
+              </div>
+              <!-- card -->
+
+              <div class="card">
+                <div class="card-header" id="headingThree3">
+                  <a class="collapsed" data-toggle="collapse" data-parent="#accordionEx" href="#collapseThree3" aria-expanded="false">
+                    <h5 class="mb-0">
+                      Ваша доставка опоздала, как можно помочь вам улучшить свой уровень сервиса?
+                      <i class="icon icon--arrow-d"></i>
+                    </h5>
+                  </a>
+                </div>
+
+                <!-- Card body -->
+                <div id="collapseThree3" class="collapse" role="tabpanel" aria-labelledby="headingThree3" data-parent="#accordionEx">
+                  <div class="card-body">
+                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3
+                    wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum
+                    eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla
+                    assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred
+                    nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer
+                    farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus
+                    labore sustainable VHS.
+                  </div>
+                </div>
+              </div>
+              <!-- card -->
+            </div>
+            <!-- Accordion wrapper -->
+          </div>
+          <!-- /.col-8 -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.faq -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('layout/partials/footer.php') ?>
