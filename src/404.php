4241db43d144966495c790e04ae3de8ee3f0e3d2
acr0matic <work.acr0matic@gmail.com>
2022-09-06 17:35:12 +0300

    feat: страница "404"

---
+++ src/404.php
@@ -0,0 +1,34 @@
+<?php include_once('layout/partials/header.php') ?>
+
+<main class="page">
+  <section id="not-found" class="section">
+    <div class="container">
+      <div class="row justify-content-center text-center">
+        <div class="col-6 border">
+          <div class="not-found">
+            <div class="page__header">
+              <h1 class="page__title">
+                404
+                <br>
+                <span>Страница не найдена</span>
+              </h1>
+            </div>
+            <!-- /.section__header -->
+
+            <p class="page__description mb-4">
+              Неправильно набран адрес или такая страница больше не существует
+            </p>
+
+            <a href="" class="btn btn-primary">Перейти на главную</a>
+          </div>
+          <!-- /.not-found -->
+        </div>
+        <!-- /.col-6 -->
+      </div>
+      <!-- /.row -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('layout/partials/footer.php') ?>
