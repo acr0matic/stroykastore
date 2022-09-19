b4f857e116898a38821515eea54afe750a4c6a55
acr0matic <work.acr0matic@gmail.com>
2022-09-19 20:21:24 +0300

    feat: страница "заказы"

---
+++ src/history.php
@@ -0,0 +1,198 @@
+<?php include_once('layout/partials/header.php') ?>
+
+<main class="page">
+  <section id="heading" class="section">
+    <div class="heading">
+      <div class="container container--wide">
+        <div class="container">
+          <div class="heading__content">
+            <div class="page__breadcrumb">
+              <nav aria-label="breadcrumb">
+                <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="#">Главная</a></li>
+                  <li class="breadcrumb-item active" aria-current="page">Заказы</li>
+                </ol>
+              </nav>
+            </div>
+            <!-- /.page__breadcrumb -->
+
+            <div class="page__header">
+              <h1 class="page__title">
+                Заказы
+              </h1>
+            </div>
+            <!-- /.page__header -->
+          </div>
+          <!-- /.heading__content -->
+        </div>
+        <!-- /.container -->
+
+        <div style="background-image: url('img/page/history/heading/background.jpg');" class="heading__background"></div>
+      </div>
+      <!-- /.container container--wide -->
+    </div>
+    <!-- /.heading -->
+  </section>
+
+  <section id="history" class="section">
+    <div class="container p-sm-0">
+      <div class="history mb-n3">
+        <div class="cart-card card mb-3">
+          <div class="row m-0">
+            <div class="col-12 col-sm-4 col-md-3 col-xl-2 border-right-0 border-sm-right p-0">
+              <picture class="cart-card__picture picture picture--contain border-bottom border-sm-bottom-0">
+                <img class="picture__media lazy" data-src="" src="img/content/product/1.jpg" alt="">
+              </picture>
+            </div>
+            <!-- /.col-2 -->
+
+            <div class="col-12 col-sm-8 col-md-5 col-lg-5 col-xl-6 border-md-right p-3">
+              <h3 class="cart-card__title">Плита OSB-3 Kronospan 2500 х 1250 х 9 мм, хвойные породы</h3>
+              <div class="cart-card__price">995 ₽</div>
+              <div class="cart-card__count">2 шт.</div>
+            </div>
+            <!-- /.col-6 -->
+
+            <div class="col-12 col-md-4">
+              <div class="row border-top border-md-0">
+                <div class="col-12 col-sm-6 col-md-12 col-xl-6 p-3 mb-n3 mb-sm-0 border-right border-md-right-0">
+                  <div class="mb-1"><b>Заказ от 24 мая</b></div>
+                  <div>34078988-0047</div>
+                </div>
+                <!-- /.col-2 -->
+
+                <div class="col-12 col-sm-6 col-md-12 col-xl-6 p-3">
+                  <div class="mb-1"><b>Статус</b></div>
+                  <div>Оплачен</div>
+                </div>
+                <!-- /.col-2 -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!-- /.col-4 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.cart-card-->
+
+        <div class="cart-card card mb-3">
+          <div class="row m-0">
+            <div class="col-12 col-sm-4 col-md-3 col-xl-2 border-right-0 border-sm-right p-0">
+              <picture class="cart-card__picture picture picture--contain border-bottom border-sm-bottom-0">
+                <img class="picture__media lazy" data-src="" src="img/content/product/1.jpg" alt="">
+              </picture>
+            </div>
+            <!-- /.col-2 -->
+
+            <div class="col-12 col-sm-8 col-md-5 col-lg-5 col-xl-6 border-md-right p-3">
+              <h3 class="cart-card__title">Плита OSB-3 Kronospan 2500 х 1250 х 9 мм, хвойные породы</h3>
+              <div class="cart-card__price">995 ₽</div>
+              <div class="cart-card__count">2 шт.</div>
+            </div>
+            <!-- /.col-6 -->
+
+            <div class="col-12 col-md-4">
+              <div class="row border-top border-md-0">
+                <div class="col-12 col-sm-6 col-md-12 col-xl-6 p-3 mb-n3 mb-sm-0 border-right border-md-right-0">
+                  <div class="mb-1"><b>Заказ от 24 мая</b></div>
+                  <div>34078988-0047</div>
+                </div>
+                <!-- /.col-2 -->
+
+                <div class="col-12 col-sm-6 col-md-12 col-xl-6 p-3">
+                  <div class="mb-1"><b>Статус</b></div>
+                  <div>Оплачен</div>
+                </div>
+                <!-- /.col-2 -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!-- /.col-4 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.cart-card-->
+
+        <div class="cart-card card mb-3">
+          <div class="row m-0">
+            <div class="col-12 col-sm-4 col-md-3 col-xl-2 border-right-0 border-sm-right p-0">
+              <picture class="cart-card__picture picture picture--contain border-bottom border-sm-bottom-0">
+                <img class="picture__media lazy" data-src="" src="img/content/product/1.jpg" alt="">
+              </picture>
+            </div>
+            <!-- /.col-2 -->
+
+            <div class="col-12 col-sm-8 col-md-5 col-lg-5 col-xl-6 border-md-right p-3">
+              <h3 class="cart-card__title">Плита OSB-3 Kronospan 2500 х 1250 х 9 мм, хвойные породы</h3>
+              <div class="cart-card__price">995 ₽</div>
+              <div class="cart-card__count">2 шт.</div>
+            </div>
+            <!-- /.col-6 -->
+
+            <div class="col-12 col-md-4">
+              <div class="row border-top border-md-0">
+                <div class="col-12 col-sm-6 col-md-12 col-xl-6 p-3 mb-n3 mb-sm-0 border-right border-md-right-0">
+                  <div class="mb-1"><b>Заказ от 24 мая</b></div>
+                  <div>34078988-0047</div>
+                </div>
+                <!-- /.col-2 -->
+
+                <div class="col-12 col-sm-6 col-md-12 col-xl-6 p-3">
+                  <div class="mb-1"><b>Статус</b></div>
+                  <div>Оплачен</div>
+                </div>
+                <!-- /.col-2 -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!-- /.col-4 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.cart-card-->
+
+        <div class="cart-card card mb-3">
+          <div class="row m-0">
+            <div class="col-12 col-sm-4 col-md-3 col-xl-2 border-right-0 border-sm-right p-0">
+              <picture class="cart-card__picture picture picture--contain border-bottom border-sm-bottom-0">
+                <img class="picture__media lazy" data-src="" src="img/content/product/1.jpg" alt="">
+              </picture>
+            </div>
+            <!-- /.col-2 -->
+
+            <div class="col-12 col-sm-8 col-md-5 col-lg-5 col-xl-6 border-md-right p-3">
+              <h3 class="cart-card__title">Плита OSB-3 Kronospan 2500 х 1250 х 9 мм, хвойные породы</h3>
+              <div class="cart-card__price">995 ₽</div>
+              <div class="cart-card__count">2 шт.</div>
+            </div>
+            <!-- /.col-6 -->
+
+            <div class="col-12 col-md-4">
+              <div class="row border-top border-md-0">
+                <div class="col-12 col-sm-6 col-md-12 col-xl-6 p-3 mb-n3 mb-sm-0 border-right border-md-right-0">
+                  <div class="mb-1"><b>Заказ от 24 мая</b></div>
+                  <div>34078988-0047</div>
+                </div>
+                <!-- /.col-2 -->
+
+                <div class="col-12 col-sm-6 col-md-12 col-xl-6 p-3">
+                  <div class="mb-1"><b>Статус</b></div>
+                  <div>Оплачен</div>
+                </div>
+                <!-- /.col-2 -->
+              </div>
+              <!-- /.row -->
+            </div>
+            <!-- /.col-4 -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.cart-card-->
+      </div>
+      <!-- /.history -->
+    </div>
+    <!-- /.container -->
+  </section>
+</main>
+
+<?php include_once('layout/partials/footer.php') ?>
